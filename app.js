53792ebac84bd92766f1b567e4d746c01393b370
Axel Ramirez <142461039+AxelRam03@users.noreply.github.com>
2026-09-12 09:51:50 -0600

    Delete app.js

--- app.js
+++
@@ -1,1 +0,0 @@
-const menu=document.querySelector('.hamb'),nav=document.querySelector('nav');menu?.addEventListener('click',()=>nav.classList.toggle('open'));document.querySelectorAll('nav a').forEach(a=>a.addEventListener('click',()=>nav.classList.remove('open')));
