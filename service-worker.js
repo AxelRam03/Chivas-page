9a00823fcc286e6d5065b231e23ce89ecf982d0a
Axel Ramirez <142461039+AxelRam03@users.noreply.github.com>
2026-09-12 16:51:19 -0600

    Cache new PEYE branding assets

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='peye-tours-v4';
-const ASSETS=['./','./index.html','./css/styles.css','./css/chivas-upgrade.css','./js/app.js','./js/upgrade.js','./manifest.json','./assets/chivas-toluca.jpeg'];
+const CACHE='peye-tours-v5';
+const ASSETS=['./','./index.html','./css/styles.css','./css/chivas-upgrade.css','./css/peye-branding.css','./js/app.js','./js/upgrade.js','./manifest.json','./assets/chivas-toluca.jpeg','./assets/peye-mark.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
