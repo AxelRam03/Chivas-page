26c69fac017d6146336602102e56dde105ddcdaf
Axel Ramirez <142461039+AxelRam03@users.noreply.github.com>
2026-09-12 14:47:18 -0600

    Actualizar cache para la nueva experiencia Chivas PEYE

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-const CACHE='peye-tours-v1';
-const ASSETS=['./','./index.html','./css/styles.css','./js/app.js','./manifest.json','./assets/chivas-toluca.jpeg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+const CACHE='peye-tours-v4';
+const ASSETS=['./','./index.html','./css/styles.css','./css/chivas-upgrade.css','./js/app.js','./js/upgrade.js','./manifest.json','./assets/chivas-toluca.jpeg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
